aafcbb1d3d6095c3389d445b44296eb935a0d831
ALITECH <109954579+Alitech-lab@users.noreply.github.com>
2022-08-19 18:42:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_and_odd_numbers_between_1_to_35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd number from 1 to 35");
+            for (int i = 1; i <= 35; i++)
+            {
+                if (i % 2 != 0)
+
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Even number from 1 to 35");
+            for (int i = 1; i <= 35; i++)
+            {
+                if (i % 2 == 0)
+
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
